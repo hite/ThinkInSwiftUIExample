45b74395563707ecd31f4b6d3053b64002d0e772
hite <xiangheka@163.com>
2019-12-15 12:25:38 +0800

    add:  新增字体的默认值，试验

--- ThinkInSwiftUIExample/Charter-suffix_1/FindDefaultValues.swift
+++ ThinkInSwiftUIExample/Charter-suffix_1/FindDefaultValues.swift
@@ -11,11 +11,12 @@ import SwiftUI
 struct FindDefaultValues: View {
     var body: some View {
         VStack {
+                        // padding
             HStack(alignment: .top, spacing: 0){
                 Text("lalal").padding().background(Color.red)
                 Text("lalal").padding(16).background(Color.red)
             }
-            
+
             HStack(alignment: .top, spacing: 8){
                Image(systemName: "chevron.right.circle")
                .padding()
@@ -31,6 +32,30 @@ struct FindDefaultValues: View {
                Image(systemName: "chevron.right.circle")
                .padding(16)
                .background(Color.green)
+            }
+            // font
+            Group {
+                HStack {
+                    Text("default, body")
+                    Text("default, body").font(.body)
+                    Text("default, body").font(Font.custom("mybody", size: 17.6))
+                }
+                HStack {
+                    Text("largeTitle").font(.largeTitle)
+                    Text("largeTitle").font(Font.system(size: 34, weight: .regular, design: .default))
+                }
+                HStack {
+                    Text("caption").font(.caption)
+                    Text("caption").font(Font.system(size: 12, weight: .regular, design: .default))
+                }
+                HStack {
+                    Text("title").font(.title)
+                    Text("title").font(Font.system(size: 28, weight: .regular, design: .default))
+                }
+                HStack {
+                    Text("headline").font(.headline)
+                    Text("headline").font(Font.system(size: 17.0, weight: .medium, design: .default))
+                }
             }
         }
     }
